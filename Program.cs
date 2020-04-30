e14da328953a0b78409edb91623455943a1a2ba3
Javier <javiercangahualamarquez@gmail.com>
2020-04-29 20:33:33 -0500

    se creo el archivo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pruebagit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
